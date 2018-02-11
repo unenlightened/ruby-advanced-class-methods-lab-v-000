46640362800351028bf6e4ae69902efa293a87ae
Chely Ho <adept_inept@yahoo.com>
2018-02-11 20:04:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.all.include?(name)
-      self.find_by_name(name)
+    if self.find_by_name(name)
     else
       self.create_by_name(name)
     end
