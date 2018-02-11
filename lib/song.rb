bdb5d5e80c1143412463577d61aa33aa5dca12a5
Chely Ho <adept_inept@yahoo.com>
2018-02-11 20:06:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,10 +33,6 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    song = self
-    if self.find_by_name(name)
-    else
-      self.create_by_name(name)
-    end
+    self.find_by_name(name)
   end
 end
